586be3c3aa25e65f31987df535acffe02b995c00
ObiamakaMaria <obiamaka0101@gmail.com>
2024-03-15 14:32:49 +0000

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+The type-annotated function floor which takes a float n & returns the floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the float
+    """
+    return int(n)
