238786cd95e828ca0b172b6b5cdcd64dba40f301
ObiamakaMaria <obiamaka0101@gmail.com>
2024-03-15 14:26:12 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+The type-annotated function concat takes a string str1 & string str2
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns a concatenated string(str1str2)
+    """
+    return str1 + str2
