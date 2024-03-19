41d57f01128e2efc6b6363ca6ca3cd940e9f701a
ObiamakaMaria <obiamaka0101@gmail.com>
2024-03-19 21:30:30 +0000

    2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' An implementation of asynchronous programming as in PEP 525'''
+''' An implementation of asynchronous programming '''
 
 
 import asyncio
@@ -10,7 +10,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''The asynchronous generator function'''
+    '''An asynchronous generator function'''
     start = time.perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(),
                          async_comprehension(), async_comprehension())
