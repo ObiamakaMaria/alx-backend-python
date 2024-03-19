6a18ce82681e8cae47ad35f340e92995149ceb79
ObiamakaMaria <obiamaka0101@gmail.com>
2024-03-19 21:09:02 +0000

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-''' An implementation of Asynchronous Comprehension '''
+'''Asynchronous Comprehension as in PEP 525'''
 
 
 import asyncio
-import random
 from typing import Generator
+import random
+
 
 async def async_generator() -> Generator[float, None, None]:
-    '''An asynchronous generator function'''
+    '''The asynchronous generator function'''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
