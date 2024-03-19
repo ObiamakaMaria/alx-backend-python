a1b87183c5319f7a362f206d7ca8e2921df96144
ObiamakaMaria <obiamaka0101@gmail.com>
2024-03-19 21:16:02 +0000

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' An implementation of asynchronous programming '''
+'''Asynchronous Comprehension Implementation'''
 
 
 import asyncio
@@ -11,5 +11,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    '''An  asynchronous generator function'''
-    return [val async for val in async_generator()]
+    '''An asynchronous generator function'''
+    return [val async for val in async_generator()]i
